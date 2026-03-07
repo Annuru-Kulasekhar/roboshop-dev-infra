affc8efbc3aa94c65f044ad4567398a3003a5979
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-07 20:30:06 +0530

    actions

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,36 +1,11 @@
-resource "aws_route53_record" "mongodb" {
+resource "aws_route53_record" "database" {
+  for_each = aws_instance.database
   zone_id = var.zone_id
-  name    = "mongodb-${var.environment}.${var.domain_name}"
+  name    = "$(each.key)-${var.environment}.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = [aws_instance.mongodb.private_ip]
+  records = [each.value.private_ip]
   #if already existing record
   allow_overwrite = true
 }
 
-resource "aws_route53_record" "redis" {
-  zone_id = var.zone_id
-  name    = "redis-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.redis.private_ip]
-  allow_overwrite = true
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone_id
-  name    = "mysql-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.mysql.private_ip]
-  allow_overwrite = true
-}
-
-/* resource "aws_route53_record" "rabbitmq" {
-  zone_id = var.zone_id
-  name    = "rabbitmq-${var.environment}.${var.domain_name}"
-  type    = "A"
-  ttl     = "1"
-  records = [aws_instance.rabbitmq.private_ip]
-  allow_overwrite = true
-} */
