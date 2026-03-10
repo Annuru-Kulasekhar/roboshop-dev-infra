eaa17840f26f6b64aa2ea064096fb98a200a92f0
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-10 19:40:35 +0530

    actions

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -8,5 +8,6 @@ locals {
     ssh_password = data.aws_ssm_parameter.ssh_password.value
     private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_id.value)[0]
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
 
 }
