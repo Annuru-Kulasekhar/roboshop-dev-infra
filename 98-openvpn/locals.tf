2276d57b44b09f324a9ec55aba1bdaded570c147
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-24 19:17:26 +0530

    actions

--- 98-openvpn/locals.tf
+++ 98-openvpn/locals.tf
@@ -4,7 +4,6 @@ locals {
         Environment = var.environment
         Terraform = "true"
     }
-    ami_id = data.aws_ami.openvpn.id
-    public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_id.value)
+    public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_id.value)[0]
     openvpn_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
 }
